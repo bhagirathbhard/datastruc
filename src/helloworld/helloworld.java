a07d3c92b51bcfe59862d0a02f857ab8fcc369ec
bhagirathbhardwaj <bhagirathbhardwaj@wit-c02gm2q9djwr>
2018-05-01 20:17:16 -0400

    try

---
+++ src/helloworld/helloworld.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class helloworld {
+	
+	//Linked List
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		System.out.println("Getting back on Java");
+	}
+
+}
